12ad369b1a303a01ec5af3796ca7b9108901670e
Gamcas98 <40047389+Gamcas98@users.noreply.github.com>
2019-07-20 02:07:20 -0600

    Logica terminada

---
+++ Practica 1/js/analisis.js
@@ -0,0 +1,76 @@
+
+
+var textoEntrada= document.getElementById("texto");//obtenemos la palabra ingresada en la pagina
+
+var ascii=0;//guarda el valor ascii de cada caracter de la palabra ingresada
+var tipo;//guarda el tipo de token de la palabra ingresada
+var bandera=false;//nos sirven para llegar a un estado de error
+
+function analizar(){
+
+    bandera=false;//reinicar la varible cada ves que analizamos
+    
+    for(var i=0; i<textoEntrada.value.length; i++){//recorremos la longitud de la palabra ingresada
+        ascii=textoEntrada.value.charCodeAt(i);//convertimos a codigo ascii el caracter analizado
+        tipo=comprobarTipo(ascii);//comprobamos el token y se lo asignamos a tipo
+        if(tipo==="error"){//cuando lleguemos a un estado de error
+            bandera=true//usamos nuestra bandera
+        }
+    }
+
+    if(!bandera){// la bandera se encuentre fuera del estado de error (true)
+        if(tipo==="letra" && bandera==false){//si tipo nos devolvio token letra
+            imprimir("correctas","Identificador");
+        }else if(tipo==="digito" && bandera==false){//si nos devuelve token digito
+            imprimir("correctas","Digito");
+        }else if(tipo==="simbolo" && bandera==false){//si nos devuelve token simbolo
+            imprimir("correctas","Simbolo");
+        }
+    }else{//si la bandera esta en estado de error (false)
+    imprimir("incorrectas","Error");
+    }  
+}
+
+
+function imprimir(tabla,tipo){//metodo para llenar las listas de las palabras analizadas
+    document.getElementById(tabla).innerHTML =document.getElementById(tabla).innerHTML + "<li class=\"list-group-item\">"+textoEntrada.value+"-->"+tipo+"</li>";
+}
+//ahora si se viene lo chido :v
+
+function comprobarTipo(texto){//nos devuelve un string indicando el tipo de token
+    //comparamos que la primera posicion de la palabra sea una letra mayuscula o minuscula
+    if((textoEntrada.value.charCodeAt(0)>=97 && textoEntrada.value.charCodeAt(0)<=122) || (textoEntrada.value.charCodeAt(0)>=65 && textoEntrada.value.charCodeAt(0)<=90) ){
+        //si cumple con ser una letra el primer caracter
+        //comparamos que los siguientes caracteres sigan siendo letras o numeros
+        if((texto>=97 && texto<=122) || (texto>=65 && texto<=90) || texto>=48 && texto<=57){
+		    return("letra");
+        
+        }else {//si algun caracter no cumple nos vamos a un estado de error
+           
+            return("error");        
+        }
+    //comparamos que el primer caracter sea un numero        
+    }else if((textoEntrada.value.charCodeAt(0)>=48 && textoEntrada.value.charCodeAt(0)<=57) ){
+    
+        if(texto>=48 && texto<=57){//si cumple, continua comparando que la palabra contenga solo numeros
+            return("digito");
+        
+	    }else {
+            return("error");
+        }
+    //comparamos que el primer caracter sea un simbolo    
+    }else if(textoEntrada.value.charCodeAt(0)>=35 && textoEntrada.value.charCodeAt(0)<=37 ) {
+       
+        if(texto>=35 && texto<=37){
+            return("simbolo");
+        
+	    }else{
+            return("error");
+        } 
+    }else{//si no cumple con empezar con letra numero o simbolo, nos envia a un estado de error
+        return("error");
+    }
+
+ 
+   
+}
