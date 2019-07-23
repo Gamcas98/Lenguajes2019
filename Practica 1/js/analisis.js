ec44814ef2c90f352fe10a46bb2a238659a4d560
Gamcas98 <40047389+Gamcas98@users.noreply.github.com>
2019-07-22 20:53:04 -0600

    arreglando bugs

--- Practica 1/js/analisis.js
+++ Practica 1/js/analisis.js
@@ -9,28 +9,31 @@ var bandera=false;//nos sirven para llegar a un estado de error
 function analizar(){
     
     bandera=false;//reinicar la varible cada ves que analizamos
-    
-    for(var i=0; i<textoEntrada.value.length; i++){//vamos a recorrer caracter por caracter la palabra
-        ascii=textoEntrada.value.charCodeAt(i);//convertimos a codigo ascii el caracter analizado
-        tipo=comprobarTipo(ascii);//comprobamos el token y se lo asignamos a tipo
-        if(tipo==="error"){//cuando lleguemos a un estado de error
+    if(!textoEntrada.value==""){
+        for(var i=0; i<textoEntrada.value.length; i++){//vamos a recorrer caracter por caracter la palabra
+            ascii=textoEntrada.value.charCodeAt(i);//convertimos a codigo ascii el caracter analizado
+            tipo=comprobarTipo(ascii);//comprobamos el token y se lo asignamos a tipo
+            if(tipo==="error"){//cuando lleguemos a un estado de error
             bandera=true//usamos nuestra bandera
+            }
         }
-    }
+    
 
-    if(!bandera){// la bandera se encuentre fuera del estado de error (true)
-        if(tipo==="letra" && bandera==false){//si tipo nos devolvio token letra
+        if(!bandera){// la bandera se encuentre fuera del estado de error (true)
+            if(tipo==="letra" && bandera==false){//si tipo nos devolvio token letra
             imprimir("correctas","Identificador");
-        }else if(tipo==="digito" && bandera==false){//si nos devuelve token digito
+            }else if(tipo==="digito" && bandera==false){//si nos devuelve token digito
             imprimir("correctas","Digito");
-        }else if(tipo==="simbolo" && bandera==false){//si nos devuelve token simbolo
+            }else if(tipo==="simbolo" && bandera==false){//si nos devuelve token simbolo
             imprimir("correctas","Simbolo");
-        }
-    }else{//si la bandera esta en estado de error (false)
-    imprimir("incorrectas","Error");
-    }  
-    textoEntrada.value="";
-    textoEntrada.focus();
+            }
+        }else{//si la bandera esta en estado de error (false)
+        imprimir("incorrectas","Error");
+        }  
+        textoEntrada.value="";
+        textoEntrada.focus();
+        console.log(textoEntrada.value);
+}
 }
 
 //funcion que detecta el espacio para avisarle al compa que no lo haga
@@ -38,19 +41,20 @@ function space(event){
  
     if (event.keyCode == 32 || event.which == 32){
         textoEntrada.value="";
-        alert('no lo haga compa');  
-
+        confirm('no lo haga compa');  
+        textoEntrada.value.trim();
     }
     
 }
 
+
 //funcion para analizar la palabra al presionar enter
 function enter(event){
-    if (event.keyCode == 13 || event.which == 13){
-        analizar();
+    if (!textoEntrada.value==""){
+        if (event.keyCode == 13 || event.which == 13){
+            analizar();
+        }
     }
-    console.log(textoEntrada);
-
 }
 function imprimir(tabla,tipo){//metodo para llenar las listas de las palabras analizadas
     document.getElementById(tabla).innerHTML =document.getElementById(tabla).innerHTML + "<li class=\"list-group-item\">"+textoEntrada.value+"-->"+tipo+"</li>";
@@ -59,7 +63,8 @@ function imprimir(tabla,tipo){//metodo para llenar las listas de las palabras an
 
 function comprobarTipo(texto){//nos devuelve un string indicando el tipo de token
     //comparamos que la primera posicion de la palabra sea una letra mayuscula o minuscula
-    if((textoEntrada.value.charCodeAt(0)>=97 && textoEntrada.value.charCodeAt(0)<=122) || (textoEntrada.value.charCodeAt(0)>=65 && textoEntrada.value.charCodeAt(0)<=90) ){
+    if((textoEntrada.value.charCodeAt(0)>=97 && textoEntrada.value.charCodeAt(0)<=122) 
+    || (textoEntrada.value.charCodeAt(0)>=65 && textoEntrada.value.charCodeAt(0)<=90)){
         //si cumple con ser una letra el primer caracter
         //comparamos que los siguientes caracteres sigan siendo letras o numeros
         if((texto>=97 && texto<=122) || (texto>=65 && texto<=90) || texto>=48 && texto<=57){
@@ -86,8 +91,9 @@ function comprobarTipo(texto){//nos devuelve un string indicando el tipo de toke
         
 	    }else{
             return("error");
-        } 
-    }else{//si no cumple con empezar con letra numero o simbolo, nos envia a un estado de error
+        }
+    
+    }else{
         return("error");
     }
 
